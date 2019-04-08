5898967ae6e4cdc61782093087f529f91f6d5f6d
solomonk-ch <elhachimi.ch@gmail.com>
2019-04-08 13:26:43 +0100

    Remove rm command for get-pip

--- setup.py
+++ setup.py
@@ -18,14 +18,23 @@ def set_pip_version(o, pip):
 class Setup:
     __commandes = []
 
-    def __init__(self, pip='', python_version=''):
+    def __init__(self, pip='', python_version='', conda=False, conda_environement=''):
         self.read_commandes_list()
         commandes = self.get_commandes()
-        if len(python_version) != 0:
-            commandes[1] = re.sub('python', 'python3.6', commandes[1][0])
-        if len(pip) != 0:
+
+        if conda:
             for i in range(len(commandes)):
+                commandes[i][0] = re.sub('pip install', 'conda install --name '+conda_environement, commandes[i][0], 1)
+
+        if len(python_version) != 0:
+            commandes[1] = [re.sub('python', python_version, commandes[1][0]), '1']
+
+        if len(pip) != 0:
+            for i in range(2, len(commandes)):
                 commandes[i][0] = set_pip_version(commandes[i][0], pip)
+
+        self.__commandes = commandes
+
         print("""
      _______. _______ .___________. __    __  .______   
     /       ||   ____||           ||  |  |  | |   _  \  
@@ -41,12 +50,12 @@ class Setup:
     def set_commandes(self, new_commandes_list):
         self.__commandes = new_commandes_list
 
-    @staticmethod
-    def add_command(new_command):
+    def add_command(self, new_command):
         new_command = [new_command, 0]
         with open(os.path.join(BASE_NAME, 'data/commandes.csv'), 'a+', newline='', encoding='utf-8') as file:
             writer = csv.writer(file, delimiter=',')
             writer.writerow(new_command)
+        self.read_commandes_list()
 
     def read_commandes_list(self, delimiter=','):
         with open(os.path.join(BASE_NAME, 'data/commandes.csv'), 'r+', encoding='utf-8') as f:
@@ -59,16 +68,22 @@ class Setup:
             for p in self.get_commandes():
                 writer.writerow(p)
 
-    def start(self, updtae=False, mac=0):
+    def start(self, updtae=False, mac=100):
         commandes = list(self.get_commandes())
-        for i in range(len(commandes)):
+        print(commandes)
+        for i in range(1, len(commandes)):
             os.system(commandes[i][0])
             commandes[i][1] = 1
+
+        self.__commandes.pop(0)
+        self.__commandes.insert(0, ['commande', 'status'])
+
         if mac == 1:
+            print("MAC")
             os.system('pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.0.0-py3-none-any.whl')
-        else:
+        elif mac == 0:
+            print("Windows")
             os.system('pip install --upgrade --ignore-installed tensorflow==1.5')
-        os.system('rm -r get-pip.py')
         if updtae:
             self.__commandes.pop(0)
             self.__commandes.insert(0, ['commande', 'status'])
